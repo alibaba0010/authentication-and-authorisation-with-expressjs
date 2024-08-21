ac053a942c2261a100823d49c8747cf5749410cf
alibaba0010 <zakariyahali100@gmail.com>
2024-08-21 09:19:15 +0100

    Adding delete user functionality route

--- back-end/routes/authHandling.js
+++ back-end/routes/authHandling.js
@@ -17,5 +17,10 @@ router.get("/user", authentication, authorisation("user"), (req, res) =>
 router.put("/user", authentication, authorisation("user"), (req, res) =>
   authController.update_user_profile(req, res)
 );
-
+router.put(
+  "/delete/user",
+  authentication,
+  authorisation({ isAdmin: false }),
+  (req, res) => authController.delete_user_by_username(req, res)
+);
 module.exports = router;
